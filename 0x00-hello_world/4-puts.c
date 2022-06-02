bcf9e6ad96f090e55289660827650b26ad580cea
abelasfaw <abeloman89@gmail.com>
2022-06-02 11:55:52 -0700

    added script 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main-main function fo program
+ * Return: return 0 on success
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
