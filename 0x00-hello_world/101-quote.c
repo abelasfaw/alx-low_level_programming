653ee7cadb0fb2ec7d52a7e1aa4114b137d02c08
abelasfaw <abeloman89@gmail.com>
2022-06-02 13:50:17 -0700

    added script 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ *main- main fucntion
+ *Return: returns 1 on error
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	exit(1);
+}
