b0d0d4b8d48ac3df4dd64dc3fa365a5c38484f4a
abelasfaw <abeloman89@gmail.com>
2022-06-02 14:09:12 -0700

    fix script 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-
-			10-19\n");
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, %d-%d-%d\n",
+	2015, 10, 19);
 	exit(1);
 }
