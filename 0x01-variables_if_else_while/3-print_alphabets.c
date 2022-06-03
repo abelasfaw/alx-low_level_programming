11c211dfcd5028feccd4c8296953d0a65956c87d
abelasfaw <abeloman89@gmail.com>
2022-06-03 11:16:15 -0700

    added script 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main- main function
+ * Return: returns 0 on success
+ */
+int main(void)
+{
+	char ch = 'a';
+	char ch2 = 'A';
+
+	while (ch < 'z')
+	{
+		putchar(ch);
+		ch += 1;
+	}
+	while (ch2 < 'Z')
+	{
+		putchar(ch2);
+		ch2 += 1;
+	}
+	putchar('\n');
+	return (0);
+}
