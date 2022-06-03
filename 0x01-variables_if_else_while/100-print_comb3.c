3fe276c76137bb9304f4370f45b5dbf3d7ba0ea4
abelasfaw <abeloman89@gmail.com>
2022-06-03 13:59:27 -0700

    added script 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main- main function
+ * Return: return 0 on success
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10 ; i++)
+	{
+		for (j = 0; j < 10 ; j++)
+		{
+			if (!(i > 0 && j == 0 || i == j || (i > 1 && j < i + 1)))
+			{
+				putchar(i % 10 + '0');
+				putchar(j % 10 + '0');
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+
+}
