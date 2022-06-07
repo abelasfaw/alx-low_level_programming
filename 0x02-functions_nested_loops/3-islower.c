951da2b800de5e31a1e5d7d5debebe631e0c8fb4
abelasfaw <abeloman89@gmail.com>
2022-06-07 12:43:55 -0700

    added script 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+/**
+ * _islower- checks if character is lowercase
+ * @c: type int input to be checked
+ * Return: returns 1 if character is lowercase, else returns 0
+ */
+int _islower(int c)
+{
+	if (c > 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
