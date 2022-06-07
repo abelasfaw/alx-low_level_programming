c870ddd88cdddae5dbba3a1aee4f589847249d74
abelasfaw <abeloman89@gmail.com>
2022-06-07 15:34:57 -0700

    added script 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * print_to_98- prints natural numbers from input to 98
+ * @n: input
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n >= 98)
+		{
+			printf("%d, ",n);
+			n -= 1;
+		}
+	}
+	else 
+	{
+	
+		while ( n <= 98)
+		{
+			printf("%d, ", n);
+			n += 1;
+		}
+	}
+}
