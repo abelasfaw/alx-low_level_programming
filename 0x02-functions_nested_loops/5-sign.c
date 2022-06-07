73fece703155ebff35f67b5af66c29e018cdddaf
abelasfaw <abeloman89@gmail.com>
2022-06-07 13:09:53 -0700

    added script 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign-prints sign of a number input
+ * @n: number input to be checked
+ * Return: returns 1 if input is positive, -1 if negative and 0 if zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else 
+	{
+		_putchar('0');
+		return (0);
+	}
+}
