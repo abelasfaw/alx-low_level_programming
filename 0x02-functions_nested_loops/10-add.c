47fa5c8798498fadd7b101b8b6083e37c481ebed
abelasfaw <abeloman89@gmail.com>
2022-06-07 15:22:06 -0700

    fix script 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,9 +2,11 @@
 
 /**
  * add- adds two numbers
+ * @num1: parameter1
+ * @num2: parameter2
  * Return: sum of two numbers
  */
-int add (int num1, int num2)
+int add(int num1, int num2)
 {
 	return (num1 + num2);
 }
