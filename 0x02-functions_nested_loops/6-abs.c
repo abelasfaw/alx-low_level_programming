b01fe5940c7bfeb3759e444fb7153d2738c38fe0
abelasfaw <abeloman89@gmail.com>
2022-06-07 13:23:46 -0700

    added task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _abs- computes absolute value of a number
+ * @n: input
+ * Return: return absolute value
+ */
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (0);
+	}
+}
