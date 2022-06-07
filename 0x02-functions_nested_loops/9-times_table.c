5a1bbf9f34a079c178f56f78cb8dc61a0464dd09
abelasfaw <abeloman89@gmail.com>
2022-06-07 16:08:09 -0700

    fi script 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,12 +18,20 @@ void times_table(void)
 			{
 				_putchar((result / 10) + '0');
 				_putchar((result % 10) + '0');
+				if (counter2 < 9)
+				{
+					_putchar(',');
+				}
 			}
 			else
 			{
 				_putchar(result + '0');
+				if(counter2 < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
 			}
-			_putchar(',');
 			_putchar(' ');
 			counter2 += 1;
 		}
