eda7c19d70c47473b41c2d22739f0e682dbc4981
abelasfaw <abeloman89@gmail.com>
2022-06-09 11:19:33 -0700

    fix script 1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,6 +1,6 @@
 /**
  *_isdigit- checks if input is a digit (0-9)
- *@c- character to be checked
+ *@c: character to be checked
  *Return: returns 1 if digit, else return 0
  */
 int _isdigit(int c)
