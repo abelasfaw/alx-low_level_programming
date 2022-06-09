474ea540b7f4e4c523a3b2519b04d2c1fe8c9683
abelasfaw <abeloman89@gmail.com>
2022-06-09 11:27:04 -0700

    added script 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+/**
+ * mul- multiplies two integers
+ * @a: first number to be multiplied
+ * @b: second number to be multiplied
+ * Return: returns product of the two numbers
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
