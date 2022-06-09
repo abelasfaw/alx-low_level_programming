5d3b6844860af14ec5a947f4b00b1b893d2c800b
abelasfaw <abeloman89@gmail.com>
2022-06-09 12:13:37 -0700

    added task5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * more_numbers- prints 1-14 10 times
+ * Return:coid
+ */
+void more_numbers(void)
+{
+	int counter1 = 0;
+	int counter2 = 0;
+
+	while (counter1 < 10)
+	{
+		while (counter2 < 15)
+		{
+			if (counter2 > 10)
+			{
+				_putchar('1');
+			}
+			_putchar(counter2 % 10 + '0');
+			counter2 += 1;
+		}
+		counter2 = 0;
+		_putchar('\n');
+		counter1 += 1;
+	}
+}
