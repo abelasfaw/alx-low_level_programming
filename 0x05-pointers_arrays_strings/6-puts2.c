ccee39c73d8d912226d234408daff882ed1889be
abelasfaw <abeloman89@gmail.com>
2022-06-13 13:21:58 -0700

    added task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * puts2- prints every other character of a string
+ * @str: string input
+ * Return:void
+ */
+void puts2(char *str)
+{
+	char ch;
+	int counter = 0;
+
+	ch = str[0];
+	while (ch != '\0')
+	{
+		_putchar(ch);
+		_putchar('\n');
+		if (str[counter + 1] != '\0')
+		{
+			if (str[counter + 2] != '\0')
+			{
+				ch = str[counter + 2];
+				counter += 2;
+			}
+		}
+		else
+		{
+			break;
+		}
+
+	}
+
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,3 +4,5 @@ int _strlen(char *s);
 int _putchar(char c);
 void _puts(char *str);
 void print_rev(char *str);
+void rev_string(char *str);
+void puts2(char *str);
