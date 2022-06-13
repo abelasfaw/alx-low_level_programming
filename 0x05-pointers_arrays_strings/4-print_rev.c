aa571a21219968244f26f6bccf96440f2a6a6612
abelasfaw <abeloman89@gmail.com>
2022-06-13 10:29:01 -0700

    fix script 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,25 @@
 #include "main.h"
 /**
+ *_strlen - calculates length of a string
+ * @s: char type pointer of string
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	char ch;
+	int counter = 1;
+	int length = 0;
+
+	ch = *s;
+	while (ch != '\0')
+	{
+		length += 1;
+		ch = *(s + counter);
+		counter += 1;
+	}
+	return (length);
+}
+/**
  * print_rev- prints string in reverse
  * @s: char type pointer of string
  * Return: void
