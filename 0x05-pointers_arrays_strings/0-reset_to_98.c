6a63624742061627716864da1450da71ebbb4148
abelasfaw <abeloman89@gmail.com>
2022-06-13 08:41:41 -0700

    initialize project and added task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,6 @@
 /**
  * reset_to_98 - updates value of pointer (points to) to 98
- * *n: pointer of an int variable
+ * @n: pointer of an int variable
  * Return: void
  */
 void reset_to_98(int *n)
