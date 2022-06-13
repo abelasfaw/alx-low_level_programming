bbc45c5cf74a327c9b341709b364d635bf31f9fe
abelasfaw <abeloman89@gmail.com>
2022-06-13 09:17:18 -0700

    added task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+/**
+ * _strlen- calculates lenght of a given string
+ * @s: character type pointer
+ * Return: returns length of the string
+ */
+int _strlen(char *s)
+{
+	char ch;
+	int length = 0;
+	int counter = 1;
+
+	ch = *s;
+	while (ch != '\0')
+	{
+		length += 1;
+		ch = *(s + counter);
+		counter += 1
+	}
+	return (length);
+}
