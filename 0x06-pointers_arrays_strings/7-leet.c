70ce1e5b5f199f766723530c55634516dba6ad4d
abelasfaw <abeloman89@gmail.com>
2022-06-16 12:53:42 -0700

    added task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,33 @@
+/**
+ * leet- encodes a string to leet
+ * @s: string to encode
+ * Return: encoded string
+ */
+char *leet(char *s)
+{
+	int index1, index2;
+	char ch;
+
+	char cipher[5][3] = {{'a', 'A', '4'}, {'e', 'E', '3'}, {'o', 'O', '0'},
+		{'t', 'T', '7'}, {'l', 'L', '1'}};
+
+	index1, index2 = 0;
+	ch = *s;
+
+	while (ch != '\0')
+	{
+		while (index2 < 5)
+		{
+			if (ch == cipher[index2][0] || ch == cipher[index2][1])
+			{
+				*(s + index1) = cipher[index2][2];
+				break;
+			}
+			index2 += 1;
+		}
+		index2 = 0;
+		index1 += 1;
+		ch = *(s + index1);
+	}
+	return (s);
+}
