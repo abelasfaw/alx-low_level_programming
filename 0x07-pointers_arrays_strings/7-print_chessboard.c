03814d3758a6ebcb0c79e8577e4ec27ce8523f2f
abelasfaw <abeloman89@gmail.com>
2022-06-20 14:54:41 -0700

    fixed task 7

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -6,18 +6,19 @@
  */
 void print_chessboard(char (*a)[8])
 {
-	int row_counter = 0;
-	int column_counter = 0;
+	int row_counter;
+	int column_counter;
 
-	for (row_counter; row_counter < 8; row_counter++)
+	for (row_counter = 0; row_counter < 8; row_counter++)
 	{
-		for (column_counter; column_counter < 8; column_counter++)
+		for (column_counter = 0; column_counter < 8; column_counter++)
 		{
-			_putchar(a[i][j]);
-			if (j % 8 == 0)
+			_putchar(a[row_counter][column_counter]);
+			if (column_counter % 7 == 0 && column_counter)
 			{
 				_putchar('\n');
 			}
 		}
+		column_counter = 0;
 	}
 }
