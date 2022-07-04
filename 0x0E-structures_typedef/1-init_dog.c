ecd41826f744403c960bfcdf6913f4ca2ac8d777
abelasfaw <abeloman89@gmail.com>
2022-07-04 13:06:03 -0700

    fix task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 /**
  * init_dog - initializes a dog struct
  * @d: struct pointer of dog to initialize
@@ -8,6 +9,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+	{
+		exit(98);
+	}
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
