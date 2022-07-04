12724d304633edbb8d5c6ca9f2f258695585f72e
abelasfaw <abeloman89@gmail.com>
2022-07-04 12:40:51 -0700

    added task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef dog_H
+#define dog_H
+/**
+ * struct dog - dog struct with name, age and owner fields
+ * @name: name property
+ * @age: age property
+ * @owner: owner property
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+};
+#endif
