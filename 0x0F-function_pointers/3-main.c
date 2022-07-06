ddc3581a0d15f0ebd2c889e15f632deec51ce52e
abelasfaw <abeloman89@gmail.com>
2022-07-06 12:45:55 -0700

    fix division and modulo with 0

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,6 +1,7 @@
 #include "3-calc.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 /**
  * main - main function
  * @argc: number of command line arguments
@@ -25,8 +26,8 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	if (((argv[2] == (char *)'/' || argv[2] == (char *)'%') &&
-		atoi(argv[3]) == 0))
+	if ((strcmp(argv[2], "/") == 0 || strcmp(argv[2], "%") == 0) &&
+		atoi(argv[3]) == 0)
 	{
 		printf("Error\n");
 		exit(98);
