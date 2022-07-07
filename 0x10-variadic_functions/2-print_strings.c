42e239a784060e471595fa2fe116d1ef1517a822
abelasfaw <abeloman89@gmail.com>
2022-07-07 12:04:02 -0700

    added task 2

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,66 @@
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * print_separator - prints string separator
+ * @separator: separator to print
+ * Return void
+ */
+void print_separator(const char *separator)
+{
+	char ch;
+	int index = 0;
+
+	if (separator != NULL)
+	{
+		ch = *(separator + index);
+		while (ch != '\0')
+		{
+			printf("%c", ch);
+			index += 1;
+			ch = *(separator + index);
+		}
+	}
+}
+/**
+ * print_strings - prints string followed by separator
+ * @separator: string to be printed between inputs
+ * @n: number of strings passed
+ * Return: void
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int counter;
+	int index;
+	char ch1;
+	char *str1;
+
+	index = 0;
+	va_start(args, n);
+	for (counter = 0; counter < n; counter++)
+	{
+		str1 = va_arg(args, char *);
+		if (str1 == NULL)
+		{
+			printf("nil");
+		}
+		else
+		{
+			ch1 = *(str1 + index);
+			while (ch1 != '\0')
+			{
+				printf("%c", ch1);
+				index += 1;
+				ch1 = *(str1 + index);
+			}
+			index = 0;
+		}
+		if (separator != NULL && counter != (n - 1))
+		{
+			print_separator(separator);
+		}
+	}
+	printf("\n");
+	va_end(args);
+}
