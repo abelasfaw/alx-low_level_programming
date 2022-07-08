8ea02b5832d6b336f13237f3c2c74f6bfa5a08e6
abelasfaw <abeloman89@gmail.com>
2022-07-07 22:30:42 -0700

    added task 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,108 @@
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * _str_len- calcualtes length of a string
+ * @str: string to be processed
+ * Return: length of string
+ */
+int _str_len(const char * const str)
+{
+	int len, index;
+	char ch;
+
+	len = 0;
+	index = 0;
+
+	ch = *(str + index);
+	while (ch != '\0')
+	{
+		len += 1;
+		index += 1;
+		ch = *(str + index);
+	}
+	return (len);
+}
+/**
+ * print_string - prints string input
+ * @str: string to print
+ * Return: void
+ */
+void print_string(char *str)
+{
+	char ch;
+	int index;
+
+	index  = 0;
+	if (str == NULL)
+	{
+		printf("nil");
+	}
+	else
+	{
+		ch = *(str + index);
+		while (ch != '\0')
+		{
+			printf("%c", ch);
+			index += 1;
+			ch = *(str + index);
+		}
+	}
+}
+/**
+ * print_separator- prints ", "
+ * @index: current index of format string
+ * @str_len: length of format string
+ * Return: void
+ */
+void print_separator(int index, int str_len)
+{
+	if (index != (str_len - 1))
+	{
+		printf(", ");
+	}
+}
+/**
+ * print_all- prints char, integet, float string input
+ * @format: type ofargument passed to function
+ * Return: void
+ */
+void print_all(const char * const format, ...)
+{
+	char ch;
+	va_list args;
+	char *str;
+	int index;
+
+	index = 0;
+
+	ch = *(format + index);
+	va_start(args, format);
+	while (ch != '\0')
+	{
+		if (ch == 'c')
+		{
+			printf("%c", va_arg(args, int));
+			print_separator(index, _str_len(format));
+		}
+		else if (ch == 'i')
+		{
+			printf("%d", va_arg(args, int));
+			print_separator(index, _str_len(format));
+		}
+		else if (ch == 'f')
+		{
+			printf("%f", va_arg(args, double));
+			print_separator(index, _str_len(format));
+		}
+		else if (ch == 's')
+		{
+			str = va_arg(args, char *);
+			print_string(str);
+			print_separator(index, _str_len(format));
+		}
+		index += 1;
+		ch = *(format + index);
+	}
+	printf("\n");
+}
