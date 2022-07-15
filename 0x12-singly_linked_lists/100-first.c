65b04176dc11e7d0195290ba10ac3f76e4ba7048
abelasfaw <abeloman89@gmail.com>
2022-07-14 17:28:55 -0700

    fix text

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,6 +6,6 @@
  */
 void before(void)
 {
-	printf("You are beat! and yet, ypu must allow,\nI bore my house upon my back\n
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back\n
 			");
 }
