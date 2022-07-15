78f1d33317891a6f2ed83c0a86b17c73fad6088d
abelasfaw <abeloman89@gmail.com>
2022-07-14 17:21:50 -0700

    added task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * before- prints string before main
+ * Rrturn: void
+ */
+void before(void)
+{
+	printf("You are beat! and yet, ypu must allow,\nI bore my house upon my back\n
+			");
+}
