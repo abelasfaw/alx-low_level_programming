6c3254ff7916418cfa71b522f11f78c75aab7202
abelasfaw <abeloman89@gmail.co>
2022-07-18 13:59:18 -0700

    added task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_listint2- frees memory of listint_t type list
+ * @head: head of linked list
+ * Return: void
+ */
+void free_listint2(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
