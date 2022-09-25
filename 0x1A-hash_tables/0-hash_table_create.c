3d5660a3ca37dae9883fb2f3dbe37eb5b58911e1
abelasfaw <abeloman89@gmail.com>
2022-09-25 06:17:23 -0700

    fix task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,24 +8,29 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_node_t *array;
+	hash_node_t **array;
 	hash_table_t *hash_table;
+	unsigned long int index;
 
 	if (size == 0)
 	{
 		return (NULL);
 	}
-	array = calloc((size_t)size,  sizeof(hash_node_t *));
-	if (array == NULL)
-	{
-		return (NULL);
-	}
-	hash_table = malloc(sizeof(hash_table_t));
+	hash_table = malloc(sizeof(hash_table_t) * 1);
 	if (hash_table == NULL)
 	{
 		return (NULL);
 	}
+	array = malloc(sizeof(hash_node_t*) * size); 
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	for (index = 0; index < size; index++)
+	{
+		array[index] = NULL;
+	}
 	hash_table->size = size;
-	hash_table->array = &array;
+	hash_table->array = array;
 	return (hash_table);
 }
